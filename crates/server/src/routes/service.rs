69a2346d03a4f01fae20e787ad2cb34a80dfc5fa
Noah Too <krivahtoo@gmail.com>
2024-09-12 09:28:15 +0300

    feat: add endpoint for service logs

--- crates/server/src/routes/service.rs
+++ crates/server/src/routes/service.rs
@@ -1,18 +1,27 @@
 use axum::{
-    extract::{Path, State},
+    extract::{Path, Query, State},
     response::{IntoResponse, Response},
     routing::{get, put},
     Json, Router,
 };
 use axum_macros::debug_handler;
+use serde::Deserialize;
 use serde_json::json;
 use tracing::error;
 
 use crate::{
     auth::Claims,
-    models::service::{Service, ServiceForCreate, ServiceForUpdate},
+    models::{
+        log::Log,
+        service::{Service, ServiceForCreate, ServiceForUpdate},
+    },
     AppState,
 };
+
+#[derive(Deserialize)]
+struct Pagination {
+    limit: Option<u32>,
+}
 
 #[debug_handler]
 async fn add_service(
@@ -109,9 +118,35 @@ async fn list_services(State(state): State<AppState>) -> Response {
         .into_response()
 }
 
+#[debug_handler]
+async fn list_service_logs(
+    //_: Claims,
+    State(state): State<AppState>,
+    Path(service_id): Path<u32>,
+    pagination: Query<Pagination>,
+) -> Response {
+    match Log::list(&state.pool, service_id, pagination.limit).await {
+        Ok(logs) => Response::builder()
+            .header("Content-Type", "application/json")
+            .body(json!({ "logs": logs }).to_string())
+            .unwrap()
+            .into_response(),
+        Err(e) => {
+            error!("{e}");
+            Response::builder()
+                .header("Content-Type", "application/json")
+                .status(500)
+                .body(json!({ "message": "Internal server error" }).to_string())
+                .unwrap()
+                .into_response()
+        }
+    }
+}
+
 pub fn routes(state: AppState) -> Router {
     Router::new()
-        .route("/service", get(list_services).post(add_service))
-        .route("/service/:id", put(update_service).get(get_service))
+        .route("/services", get(list_services).post(add_service))
+        .route("/services/:id", put(update_service).get(get_service))
+        .route("/services/:id/logs", get(list_service_logs))
         .with_state(state)
 }
